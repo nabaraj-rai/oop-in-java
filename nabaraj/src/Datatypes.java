989bff8c9615cda7578e3bd987c57009d19b6382
Nabaraj Rai <115961746+nabaraj-rai@users.noreply.github.com>
2026-06-10 10:09:01 +0545

    Even Odd

---
+++ nabaraj/src/Datatypes.java
@@ -0,0 +1,21 @@
+public class Datatypes {
+    
+    public static void main(String[] args) {
+        int population = 30000000;
+        long phonenumber = 9739432126L;
+        float price = 99.99f;
+        double temp = 36.75;
+        char grade = 'A';
+        boolean isStdnt =true;
+        byte Age = 100;
+
+
+        System.out.println("Age: "+Age);
+        System.out.println("Population: "+population);
+        System.out.println("Phone NO.: "+phonenumber);
+        System.out.println("Price: "+price);
+        System.out.println("Temperature: "+temp);
+        System.out.println("Grade: "+grade);
+        System.out.println("Is Student?: "+isStdnt);
+    }
+}
